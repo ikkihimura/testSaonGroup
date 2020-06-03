8900e9e5ad203cd601bb215c01b8227fbc9a20f6
Herbert <herbert.rodriguezr@gmail.com>
2020-06-02 22:21:11 -0600

    Create The Web APP
    Add the Web app to the solution
    Adding Helper Class with the Http Client for calling the request to the Web API
    Adding a DTO for the Job Object
    Create the skeleton for the Job Controller

---
+++ WebApp/Helper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    public class ProductAPI
+    {
+        private string _apiBaseURI = "https://localhost:5001";
+        public HttpClient InitializeClient()
+        {
+            var client = new HttpClient();
+            //Passing service base url    
+            client.BaseAddress = new Uri(_apiBaseURI);
+
+            client.DefaultRequestHeaders.Clear();
+            //Define request data format    
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+    }
+
+   
+}
---
+++ WebApp/Models/JobDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Models
+{
+    public class JobDTO
+    {
+        public long IdJob { get; set; }
+        public string JobName { get; set; }
+        public string JobTitle { get; set; }
+        public string JobDescription { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
